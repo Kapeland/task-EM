c2d25ba6c873a3daead22cca4bd9657c3b295637
Kapeland <mdidkovskij@gmail.com>
2024-11-29 14:46:05 +0300

    Removed unnecessary structs.

--- internal/models/structs/music.go
+++ internal/models/structs/music.go
@@ -14,28 +14,3 @@ type FullMusicEntry struct {
 	Release time.Time `db:"release_date"`
 	Link    string    `db:"link"`
 }
-
-type TestFull struct {
-	ID          int
-	Name        string
-	Description string
-	Category    string
-	DiffLevel   int
-	Questions   []TestQuestionFull
-}
-
-type TestQuestionFull struct {
-	ID       int
-	TestID   int
-	Question string
-	IsSong   bool
-	Song     []byte
-	Answers  []QuestionAnswer
-}
-
-type QuestionAnswer struct {
-	ID         int    `db:"id"`
-	QuestionID int    `db:"question_id"`
-	Answer     string `db:"answer"`
-	IsCorrect  bool   `db:"is_correct"`
-}
